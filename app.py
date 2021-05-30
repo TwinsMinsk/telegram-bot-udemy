71bb547c715f4c1920ee3467c96e3a46b8291f00
TwinsMinsk <twinsminsk@yandex.ru>
2021-05-30 13:39:06 +0300

    Подключена база данных Postgres. Таблица : id|full_name|username|telegram_id

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
+import logging
+
 from aiogram import executor
 
-from loader import dp
+from loader import dp, db
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Устанавливаем дефолтные команды
-    await set_default_commands(dispatcher)
-
     # Уведомляет про запуск
+    logging.info("Создаем подключение к базе данных")
+    await db.create()
+
+    logging.info("Создаем таблицу пользователей")
+    await db.create_table_users()
+    logging.info("Готово.")
+
     await on_startup_notify(dispatcher)
 
 
